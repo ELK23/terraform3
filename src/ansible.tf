23ac95e7613ec615849007e7732743016b51f64a
ELK23 <vipv2496@gmail.com>
2024-12-09 13:11:27 +0000

    new3

--- src/ansible.tf
+++ src/ansible.tf
@@ -1,5 +1,5 @@
 resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/for.ini"
+  filename = "${path.module}/hosts.ini"
   content  = templatefile("${path.module}/hosts.tftpl", {
     web_vms      = var.web_vms
     db_vms       = var.db_vms
